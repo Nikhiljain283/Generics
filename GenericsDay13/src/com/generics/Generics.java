c9269d4fcf2cd4308d7d5208f2919542fee9fedc
Nikhiljain283 <nikhiljainsgrl283@gmail.com>
2022-09-07 20:56:55 +0530

    Added generics method

--- GenericsDay13/src/com/generics/Generics.java
+++ GenericsDay13/src/com/generics/Generics.java
@@ -2,47 +2,24 @@ package com.generics;
 
 public class Generics {
 	
-	public static void findMaxInteger(Integer num1, Integer num2, Integer num3) {
+	public static <E extends Comparable> void findMax(E num1, E num2, E num3) {
 		if(num1.compareTo(num2) > 0 && num1.compareTo(num3) > 0) {
-			System.out.println("maximum number is : "+num1);
+			System.out.println("maximum is : "+num1);
 		}
 		else if(num2.compareTo(num1) > 0 && num2.compareTo(num3) > 0) {
-			System.out.println("maximum number is : "+num2);
+			System.out.println("maximum is : "+num2);
 		}
 		else {
-			System.out.println("Maximum number is : "+num3);
+			System.out.println("Maximum is : "+num3);
 		}
 	}
 	
-	public static void findMaxFloat(Float num1, Float num2, Float num3) {
-		if(num1.compareTo(num2) > 0 && num1.compareTo(num3) > 0) {
-			System.out.println("maximum number is : "+num1);
-		}
-		else if(num2.compareTo(num1) > 0 && num2.compareTo(num3) > 0) {
-			System.out.println("maximum number is : "+num2);
-		}
-		else {
-			System.out.println("Maximum number is : "+num3);
-		}
-	}
-	
-	public static void findMaxString(String str1, String str2, String str3) {
-		if(str1.compareTo(str2) > 0 && str1.compareTo(str3) > 0) {
-			System.out.println("maximum string is : "+str1);
-		}
-		else if(str2.compareTo(str1) > 0 && str2.compareTo(str3) > 0) {
-			System.out.println("maximum string is : "+str2);
-		}
-		else {
-			System.out.println("Maximum string is : "+str3);
-		}
-	}
-	
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to generics");
 		
-		Generics.findMaxInteger(34, 54, 32);
-		Generics.findMaxFloat(36.4f, 45.6f, 74.6f);
-		Generics.findMaxString("red", "yellow", "blue");
+		Generics.findMax(34, 54, 32);
+		Generics.findMax(36.4f, 45.6f, 74.6f);
+		Generics.findMax("red", "yellow", "blue");
 	}	
 }
