fa925d167549658a1cfe84914894e1fc828c9ea3
Nikhiljain283 <nikhiljainsgrl283@gmail.com>
2022-09-07 20:44:44 +0530

    Added maximum string

--- GenericsDay13/src/com/generics/Generics.java
+++ GenericsDay13/src/com/generics/Generics.java
@@ -26,10 +26,23 @@ public class Generics {
 		}
 	}
 	
+	public static void findMaxString(String str1, String str2, String str3) {
+		if(str1.compareTo(str2) > 0 && str1.compareTo(str3) > 0) {
+			System.out.println("maximum string is : "+str1);
+		}
+		else if(str2.compareTo(str1) > 0 && str2.compareTo(str3) > 0) {
+			System.out.println("maximum string is : "+str2);
+		}
+		else {
+			System.out.println("Maximum string is : "+str3);
+		}
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome to generics");
 		
 		Generics.findMaxInteger(34, 54, 32);
 		Generics.findMaxFloat(36.4f, 45.6f, 74.6f);
+		Generics.findMaxString("red", "yellow", "blue");
 	}	
 }
