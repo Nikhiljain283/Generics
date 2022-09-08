56667b41e2920bd70b48b981a48ef8a5afac652a
Nikhiljain283 <nikhiljainsgrl283@gmail.com>
2022-09-08 19:09:53 +0530

    Taking more than 3 parameter.

--- GenericsDay13/src/com/generics/Generics.java
+++ GenericsDay13/src/com/generics/Generics.java
@@ -12,27 +12,34 @@ public class Generics<T extends Comparable> {
 		this.thirdInput = thirdInput;
 	}
 
-	public void findMax() {
-		Generics.findMax(firstInput, secondInput, thirdInput);
-	}
+//	public void findMax() {
+//		Generics.findMax(firstInput, secondInput, thirdInput);
+//	}
 
-	public static <E extends Comparable> void findMax(E firstInput, E secondInput, E thirdInput) {
-		if (firstInput.compareTo(secondInput) > 0 && firstInput.compareTo(thirdInput) > 0) {
-			System.out.println("maximum is : " + firstInput);
-		} else if (secondInput.compareTo(firstInput) > 0 && secondInput.compareTo(thirdInput) > 0) {
-			System.out.println("maximum is : " + secondInput);
-		} else {
-			System.out.println("Maximum is : " + thirdInput);
+	public static <E extends Comparable> void findMax(E[] inputArr) {
+		E temp;
+		for (int i = 0; i < inputArr.length; i++) {
+			for (int j = i + 1; j < inputArr.length; j++) {
+				if (inputArr[i].compareTo(inputArr[j]) > 0) {
+					temp = inputArr[i];
+					inputArr[i] = inputArr[j];
+					inputArr[j] = temp;
+				}
+			}
 		}
+		System.out.println("Maximum is : " + inputArr[inputArr.length - 1]);
 	}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to generics");
 
-		new Generics("cat", "mat", "rat").findMax();
-		new Generics(46, 98, 42).findMax();
-		new Generics(5.6f, 4.5f, 7.4f).findMax();
-		Generics.findMax(36.4f, 45.6f, 74.6f);
-		Generics.findMax("red", "yellow", "blue");
+		Integer[] intArr = { 45, 65, 48, 75, 98 };
+		Float[] floatArr = { 54.6f, 45.3f, 84.5f, 25.9f, 78.1f };
+		String[] strArr = { "red", "yellow", "blue", "green", "orange" };
+//		new Generics("cat", "mat", "rat").findMax();
+//		new Generics(46, 98, 42).findMax();
+//		new Generics(5.6f, 4.5f, 7.4f).findMax();
+		Generics.findMax(intArr);
+		Generics.findMax(floatArr);
 	}
 }
