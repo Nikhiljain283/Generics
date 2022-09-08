75a3905bb8da9e1d6b01debde24d8a5dc0c7cd6c
Nikhiljain283 <nikhiljainsgrl283@gmail.com>
2022-09-08 19:13:18 +0530

    taking more than 3 parameter

--- GenericsDay13/src/com/generics/Generics.java
+++ GenericsDay13/src/com/generics/Generics.java
@@ -12,10 +12,6 @@ public class Generics<T extends Comparable> {
 		this.thirdInput = thirdInput;
 	}
 
-//	public void findMax() {
-//		Generics.findMax(firstInput, secondInput, thirdInput);
-//	}
-
 	public static <E extends Comparable> void findMax(E[] inputArr) {
 		E temp;
 		for (int i = 0; i < inputArr.length; i++) {
@@ -36,9 +32,6 @@ public class Generics<T extends Comparable> {
 		Integer[] intArr = { 45, 65, 48, 75, 98 };
 		Float[] floatArr = { 54.6f, 45.3f, 84.5f, 25.9f, 78.1f };
 		String[] strArr = { "red", "yellow", "blue", "green", "orange" };
-//		new Generics("cat", "mat", "rat").findMax();
-//		new Generics(46, 98, 42).findMax();
-//		new Generics(5.6f, 4.5f, 7.4f).findMax();
 		Generics.findMax(intArr);
 		Generics.findMax(floatArr);
 	}
