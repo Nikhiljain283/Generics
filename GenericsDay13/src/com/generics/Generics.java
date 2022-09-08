d3054c4baac8f402d278b7874aa7ef4356c384e0
Nikhiljain283 <nikhiljainsgrl283@gmail.com>
2022-09-08 11:55:18 +0530

    Added generics class

--- GenericsDay13/src/com/generics/Generics.java
+++ GenericsDay13/src/com/generics/Generics.java
@@ -1,16 +1,30 @@
 package com.generics;
 
-public class Generics {
+public class Generics<T extends Comparable> {
 	
-	public static <E extends Comparable> void findMax(E num1, E num2, E num3) {
-		if(num1.compareTo(num2) > 0 && num1.compareTo(num3) > 0) {
-			System.out.println("maximum is : "+num1);
+	T firstInput;
+	T secondInput;
+	T thirdInput;
+	
+	Generics(T firstInput, T secondInput, T thirdInput) {
+		this.firstInput = firstInput;
+		this.secondInput = secondInput;
+		this.thirdInput = thirdInput;
+	}
+	
+	public void findMax() {
+		Generics.findMax(firstInput, secondInput, thirdInput);
+	}
+	
+	public static <E extends Comparable> void findMax(E firstInput, E secondInput, E thirdInput) {
+		if(firstInput.compareTo(secondInput) > 0 && firstInput.compareTo(thirdInput) > 0) {
+			System.out.println("maximum is : "+firstInput);
 		}
-		else if(num2.compareTo(num1) > 0 && num2.compareTo(num3) > 0) {
-			System.out.println("maximum is : "+num2);
+		else if(secondInput.compareTo(firstInput) > 0 && secondInput.compareTo(thirdInput) > 0) {
+			System.out.println("maximum is : "+secondInput);
 		}
 		else {
-			System.out.println("Maximum is : "+num3);
+			System.out.println("Maximum is : "+thirdInput);
 		}
 	}
 	
@@ -18,7 +32,9 @@ public class Generics {
 	public static void main(String[] args) {
 		System.out.println("Welcome to generics");
 		
-		Generics.findMax(34, 54, 32);
+		new Generics("cat","mat","rat").findMax();
+		new Generics(46,98,42).findMax();
+		new Generics(5.6f,4.5f,7.4f).findMax();
 		Generics.findMax(36.4f, 45.6f, 74.6f);
 		Generics.findMax("red", "yellow", "blue");
 	}	
